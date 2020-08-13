f20bfb8028897512c0cceb3ceabe83535b579ead
Mazen Abdelbasir <mazen.elba@gmail.com>
2020-08-12 20:28:29 -0400

    first commit

---
+++ build/precache-manifest.2c4a53c64d3df92a710bf2a3d639fe7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e7b3c83dcd0d056a210be5427eac32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "540faeb7a5198c2caa51",
+    "url": "/static/css/main.2f6aa74e.chunk.css"
+  },
+  {
+    "revision": "b77e38c89dcc989c59e1",
+    "url": "/static/js/2.189b4e3d.chunk.js"
+  },
+  {
+    "revision": "61eedd39499d34d7aaf09803c460ef46",
+    "url": "/static/js/2.189b4e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540faeb7a5198c2caa51",
+    "url": "/static/js/main.db6a41b4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
